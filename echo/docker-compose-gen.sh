320229b7e6633d1ee0703638352850b7401dbc74
Roman Titov <ffuugoo@users.noreply.github.com>
2023-03-01 13:51:09 +0100

    Update manual test scripts

--- echo/docker-compose-gen.sh
+++ echo/docker-compose-gen.sh
@@ -18,25 +18,34 @@ networks:
 services:
 EOF
 
+if [[ -v RUST_LOG && -n $RUST_LOG ]]
+then
+	declare ENV="$(
+		cat <<-EOF
+		    environment:
+		      - RUST_LOG=$RUST_LOG
+		EOF
+	)"
+fi
+
 for ((node = 0; node < NODES; node++))
 do
 	declare SERVICE_NAME=echo-$node
 
-	declare ADDRESS=69.42.0.$((node + 1))
+	declare ADDRESS=69.42.0.$((1 + node))
 	declare PORT=$((8000 + node))
 
 	cat <<-EOF
 	  $SERVICE_NAME:
 	    image: echo:latest
 	    command: ./echo --http 0.0.0.0:8080 --grpc 0.0.0.0:8081
-	    environment:
-	      - RUST_LOG=debug
+	    restart: always
 	    networks:
 	      echo:
 	        ipv4_address: $ADDRESS
 	    ports:
 	      - "127.0.0.1:$PORT:8080"
-	    restart: always
+	${ENV:-}
 
 	EOF
 done
---
+++ echo/query.zsh
@@ -0,0 +1,62 @@
+#!/usr/bin/env zsh
+
+set -euo pipefail
+
+function main {
+	declare -A opts=(
+		[nodes]=5
+		[local]=1
+		[docker]=''
+	)
+
+	while (( $# ))
+	do
+		case $1 in
+			--nodes)
+				opts[nodes]=$2
+				shift 2
+			;;
+
+			--local)
+				opts[local]=1
+				opts[docker]=''
+				shift
+			;;
+
+			--docker)
+				opts[docker]=1
+				opts[local]=''
+				shift
+			;;
+
+			*)
+				echo "ERROR: Unexpected argument '$1'" >&2
+				return 1
+			;;
+		esac
+	done
+
+	declare addrs=()
+
+	for ((node = 0; node < opts[nodes]; node++))
+	do
+		if [[ -v opts[local] && -n $opts[local] ]]
+		then
+			addrs+=( 127.0.0.1:$((9000 + node)) )
+		else
+			addrs+=( 69.42.0.$((1 + node)):8081 )
+		fi
+	done
+
+	declare data=${(j:, :)${:-\"${^addrs}\"}}
+
+	for (( node = 0; node < opts[nodes]; node++))
+	do
+		echo curl 127.0.0.1:$((8000 + node)) \
+			-X POST \
+			-H 'Content-Type: application/json' \
+			--data-raw "{\"nodes\": [$data]}" | jq -Rr '. as $line | (fromjson? | length) // $line'
+	done
+}
+
+main $@
