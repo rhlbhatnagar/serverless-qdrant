71dfb4d03f7697abdd02e20cc285c06bb564e52b
Rahul Bhatnagar <rhls.mailbox@gmail.com>
2023-12-23 20:38:41 +0530

    Separate read + write lambdas

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -19,7 +19,12 @@ import {
   RemovalPolicy,
   CfnOutput,
 } from "aws-cdk-lib";
-import { HttpApi } from "aws-cdk-lib/aws-apigatewayv2";
+import {
+  HttpApi,
+  HttpRoute,
+  HttpRouteKey,
+  HttpMethod,
+} from "aws-cdk-lib/aws-apigatewayv2";
 import { Vpc } from "aws-cdk-lib/aws-ec2";
 import { Lambda } from "aws-cdk-lib/aws-ses-actions";
 
@@ -49,7 +54,15 @@ export class QdrantLambdaStack extends Stack {
       },
     });
 
-    const qdrantLambda = new Function(this, "QdrantLambda", {
+    // We're using separate lambdas for read and write
+    // Ensures that in case we have multiple parallel writes,
+    // We don't run into race conditions on the network file system
+    // The write paths have a forced concurrency of 1.
+
+    // TODO: Use separate qdrant configs for the read + write lambdas
+    // because the read lambdas can be run with indexing memory of 0.
+
+    const qdrantReadLambda = new Function(this, "QdrantReadLambda", {
       runtime: Runtime.PROVIDED_AL2023,
       handler: "not.required",
       architecture: Architecture.ARM_64,
@@ -61,12 +74,45 @@ export class QdrantLambdaStack extends Stack {
       vpc: vpc,
     });
 
-    // HTTP API Gateway
+    const qdrantWriteLambda = new Function(this, "QdrantWriteLambda", {
+      runtime: Runtime.PROVIDED_AL2023,
+      handler: "not.required",
+      architecture: Architecture.ARM_64,
+      timeout: Duration.seconds(30),
+      logRetention: RetentionDays.ONE_MONTH,
+      memorySize: 3000, // 3 GB memory
+      //reservedConcurrentExecutions: 1, // Write lambda has a forced concurrency of 1, TODO, uncomment it once the concurrency limit is increased.
+      code: Code.fromAsset("../target/lambda/main_lambda/bootstrap.zip"),
+      filesystem: LambdaFilesystem.fromEfsAccessPoint(accessPoint, "/mnt/efs"),
+      vpc: vpc,
+    });
+
+    const readIntegration = new HttpLambdaIntegration(
+      "ReadIntegration",
+      qdrantReadLambda
+    );
+
+    const writeIntegration = new HttpLambdaIntegration(
+      "WriteIntegration",
+      qdrantWriteLambda
+    );
+
+    // By default, all routes go through the read integration
+    // AKA the lambda instance with full concurrency
     const httpApi = new HttpApi(this, "QdrantHttpApi", {
-      defaultIntegration: new HttpLambdaIntegration(
-        "DefaultIntegration",
-        qdrantLambda
+      defaultIntegration: readIntegration,
+    });
+
+    // Write routes should go to the write lambda,
+    // Right now, just aded the put points, path
+    // TODO: Add more paths
+    new HttpRoute(this, "WriteRoutes", {
+      httpApi: httpApi,
+      routeKey: HttpRouteKey.with(
+        "/collections/{collection_name}/points",
+        HttpMethod.PUT
       ),
+      integration: writeIntegration,
     });
 
     new CfnOutput(this, "ApiGatewayURL", {
